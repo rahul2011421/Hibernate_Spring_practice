b59587a12a22fb863621c6a5bad83e5975fbeacd
rahuly <rahulyadavv2011421@gmail.com>
2024-06-14 10:58:03 +0530

    Added CRUD Operation in spring boot

--- Spring boot/JPA/src/main/java/org/rahul/springboot/jpa/Main.java
+++ Spring boot/JPA/src/main/java/org/rahul/springboot/jpa/Main.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class Main {
@@ -18,28 +19,60 @@ public class Main {
 
 
         // Create Object of users
-        User user1=new User();
-        user1.setName("Rahul Yadav");
-        user1.setCity("bhadohi");
-        user1.setStatus("I'm java programmer");
-
-        User user2=new User();
-        user2.setName("Manogyan");
-        user2.setCity("Gynapur");
-        user2.setStatus("Bio Stuent");
+//        User user1=new User();
+//        user1.setName("Rahul Yadav");
+//        user1.setCity("bhadohi");
+//        user1.setStatus("I'm java programmer");
+//
+//        User user2=new User();
+//        user2.setName("Manogyan");
+//        user2.setCity("Gynapur");
+//        user2.setStatus("Bio Stuent");
 
 
         //Saving single user
 //        User user=userRepository.save(user1);
 //        System.out.println(user1);
 
-        // Saving multiple user
-        List<User> users=new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        List<User>result=(List<User>) userRepository.saveAll(users);
-        result.forEach(user -> {
-            System.out.println(user);
+//        // Saving multiple user
+//        List<User> users=new ArrayList<>();
+//        users.add(user1);
+//        users.add(user2);
+//        List<User>result=(List<User>) userRepository.saveAll(users);
+//        result.forEach(user -> {
+//            System.out.println(user);
+//        });
+
+        //Read the single data in optional containing
+//        Optional<User> user=userRepository.findById(1);
+//        System.out.println(user);
+        //Read the all data in optional containing
+//        Iterable<User> userIterable=userRepository.findAll();
+//        userIterable.forEach(user->{
+//            System.out.println(user);
+//        });
+
+        //update the value but for that we need to get the data first, update it and then save it
+//        Optional<User> optional=userRepository.findById(1);
+//        User user=optional.get();
+//        System.out.println(user);
+//        user.setName("Ajay k");
+//        System.out.println(user);
+//        userRepository.save(user);
+//        System.out.println(user);
+
+
+        //deleting the single item
+//        userRepository.deleteById(1);
+//        Optional<User> user=userRepository.findById(1);
+//        System.out.println(user);
+
+        //delete all item
+        userRepository.deleteAll();
+        Iterable<User> itr=userRepository.findAll();
+        itr.forEach(user1 -> {
+            System.out.println(user1);
         });
+
     }
 }
